a4c25149b728bba61ff2b55d3334b37c9a52976a
osha7 <ogroetz@gmail.com>
2020-10-14 15:30:21 -0600

    first commit - transferred from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const PokemonList = (props) => {
+  const handleClick = (pokemon) => {
+    // console.log('here')
+    props.setSelectedPokemon(pokemon);
+  };
+  return (
+    <>
+      <h3>All Pokemon</h3>
+      <ul>
+        {props.pokemons.map((pokemon) => (
+          <li key={pokemon.name}>
+            <a
+              onClick={() => {
+                handleClick(pokemon);
+              }}
+            >
+              {pokemon.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+// ({destructured})
+const PokemonDetail = ({ pokemon }) => {
+  if (!pokemon) {
+    return null;
+  }
+  return <h3>{pokemon.name}</h3>;
+};
+
+export default function App() {
+  // const [pokemons, setPokemons] = useState([{name: "oshaSaur"}])
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const pokemonApi = "https://pokeapi.co/api/v2/pokemon";
+  useEffect(() => {
+    fetch(pokemonApi)
+      .then((resp) => resp.json())
+      // .then(data => console.log(data.results))
+      .then((data) => setPokemons(data.results));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemons</h1>
+      <ul style={{ color: "lightgrey", fontSize: "16px" }}>
+        <li>1. Show a list of Pokemon</li>
+        <li>2. When I click a Pokemon name, show details about that Pokemon</li>
+        <li>
+          3. <b>Bonus:</b> Search Filter
+        </li>
+        <li>
+          4. <b>Bonus:</b> Additional Information from another API
+        </li>
+      </ul>
+      <PokemonList
+        pokemons={pokemons}
+        setSelectedPokemon={setSelectedPokemon}
+      />
+      <PokemonDetail pokemon={selectedPokemon} />
     </div>
   );
 }
-
-export default App;
